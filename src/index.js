b76165f20b255c028c0a17d7e602c12c3e254352
Steve <steve.maina@student.moringaschool.com>
2022-08-26 13:54:44 +0300

    Complete Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ // your code here
+ let form = document.querySelector("form");
+ form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  let data =document.querySelector("#new-task-description" ).value
+  showToDo(e.target.data);
+  form.reset();
+ })
+
+})
+ function showToDo(toDo=document.querySelector("#new-task-description" ).value){
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.style.color="yellow";
+  btn.style.background="red";
+  btn.textContent="x";
+  
+  li.textContent=`${toDo}`
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+  btn.addEventListener("click", deleteList)
+
+ }
+function deleteList(e){
+  e.target.parentNode.remove();
+}
+ 
+
